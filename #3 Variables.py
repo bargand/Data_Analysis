c104a1626a812c702d9a61f47f150ca375589505
Debargha Nandi <debarghanandi.india@gmail.com>
2025-06-20 04:12:30 +0530

    U

---
+++ #3 Variables.py
@@ -0,0 +1,43 @@
+#Data types = string, intiger, boolian, none
+
+#For Numaric Data
+# INT
+a = 1
+print("the type of a is", type(a))
+
+#FLOAT
+b = 90.89
+print("the type of a is", type(b))
+
+#COMPLEX
+c = complex(8, 4)
+print("the type of a is", type(c))
+
+#FOR STRING
+d = "debargha"
+print("the type of a is", type(d))
+
+#FOR BOOLIAN
+e = True
+print("the type of a is", type(e))
+
+#LIST(this is array, we can use object in array)
+arr = [1,2,3,"debargha", ["tiger", "panda"], {"Riya": 32}]
+print(arr)
+print("the type of a is", type(arr))
+
+
+#tuple(this is array, we can use object in array)
+tup1 = {"debargha", "Riya", 89, 9}
+# tup2 = {{"debargha", "riya"}, {"Prabir", "supriya"}}
+print(tup1)
+# print(tup2)
+print("the type of a is", type(tup1))
+# print("the type of a is", type(tup2))
+
+
+#dictonary
+dict = {"debargha": 23, "Riya": 32, "prabir": 53, "supriya": 45}
+print(dict)
+print("the type of dic is", type(dict))
+
