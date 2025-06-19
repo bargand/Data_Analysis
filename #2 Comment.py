1904e2c03ba20c1e46baa8fdf2d114b08a92f70d
Debargha Nandi <debarghanandi.india@gmail.com>
2025-06-20 03:48:03 +0530

    u

---
+++ #2 Comment.py
@@ -0,0 +1,19 @@
+# one line comment
+print("I am a Good Boy")
+print("my age is 23")
+
+'''
+multiline
+comment
+'''
+
+#escape sequence
+
+#Newline escape sequence
+print("I am a Good Boy \nand my age is 23")
+
+#backslash for double coat
+print("I am a \"Good Boy\" and my age is 23")
+
+print("debargha", "riya", 45, 98)
+print("debargha", "riya", 45, 98, sep=" $ ")
