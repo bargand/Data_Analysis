67a12275e3ead4330f479e30879489cd351e0bfd
Debargha Nandi <debarghanandi.india@gmail.com>
2025-06-24 21:48:43 +0530

    u

---
+++ #5 Typecasting.py
@@ -0,0 +1,14 @@
+a = "4"
+b = "7"
+
+print(a+b)
+print(int(a)+int(b))
+
+# There are many typecasting function like: int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict() etc, they will help to convert one datatype to another datatype.
+
+#this is two types: explicit typecasting(we do), implicit typecasting(python automatically do)
+
+c = 1.9
+d = 8
+
+print(c+d)
---
+++ #6 Taking Input.py
@@ -0,0 +1,6 @@
+a= input("Enter your name: ")
+b= input("Enter your father's name: ")
+c= input("Enter your mother's name: ")
+d= input("Enter your sister's name: ")
+
+print(a, b, c, d)
