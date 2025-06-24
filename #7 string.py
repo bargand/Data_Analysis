286c09159e7fa48e9d43f82d1f40077cda01e7d7
Debargha Nandi <debarghanandi.india@gmail.com>
2025-06-24 22:34:46 +0530

    u

---
+++ #7 string.py
@@ -0,0 +1,15 @@
+name = "Debargha"
+title = 'Nandi'
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+# print(name[8]) # this throuhg an error because there was no element in index 8
+
+for i in title:
+    print(i)
