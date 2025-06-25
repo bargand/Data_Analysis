8a0e709f1ba0c95e68dac2c9250f177758833fc7
Debargha Nandi <debarghanandi.india@gmail.com>
2025-06-25 21:12:19 +0530

    u

---
+++ #8 String slicing.py
@@ -0,0 +1,12 @@
+name = "Debargha, Riya, Prabir, Supriya"
+
+print(name[0:8])
+#or
+print(name[:8])
+print(name[:-8])
+
+print(len(name))
+
+nm = "harry"
+print(len(nm))
+print(nm[-4:-2])
---
+++ #9 String Operation.py
@@ -0,0 +1,16 @@
+a = "Debargha Nandi, Riya Nandi, Prabir kumar Nandi !!"
+
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+
+print(a.replace("Debargha", "Tubai"))
+print(a.split())
+
+print(a.count("Nandi"))
+
+print(a.endswith("!!"))
+
+print(a.find("N"))
+
+print(a.index("N"))
