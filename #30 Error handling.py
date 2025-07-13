71755b002b41e9c19346a99a500a07d7606f4cc4
Debargha Nandi <debarghanandi.india@gmail.com>
2025-07-14 01:06:39 +0530

    u

---
+++ #30 Error handling.py
@@ -0,0 +1,28 @@
+'''
+for i in range(1 , 11):
+    print(a*i)
+
+
+
+a = int(input("Enter a number"))
+try:
+    for i in range(1 , 11):
+        print(a*i)
+except ValueError:
+    print("Value error")
+
+'''
+
+try:
+    print(x)
+except:
+    print("value error")
+
+
+
+
+
+
+
+
+
