a7941ae8d4bb6f263ec773080379c8de54960c35
Debargha Nandi <debarghanandi.india@gmail.com>
2025-07-18 12:38:07 +0530

    u

---
+++ #41 Math.py
@@ -0,0 +1,178 @@
+import math
+
+'''
+#Basic Functions
+
+#min and max fumction can be user for find the minimum and maximum value in the datasate.
+x = min(5,10,15,20)
+y = max(5,10,15,20)
+
+print(x)
+print(y)
+
+#ceil is used to showing the upward direction nearest intiger
+
+x = math.ceil(4.8)
+print(x)
+
+#floor is used to show downdard nearest intiger
+
+x = math.floor(4.8)
+print(x)
+
+#trunc fumction is used for removing the decimel part in the data
+
+x = math.trunc(4.7)
+print(x)
+
+#fabs is used for showing the absolute value
+
+x = math.fabs(-5.9)
+print(x) 
+
+#sqrt is used for square root
+
+x = math.sqrt(64)
+print(x)
+
+'''
+
+'''
+#Trigonometry Functions
+
+x = math.sin(90)
+print(x)
+
+y = math.cos(90)
+print(y)
+
+z = math.tan(90)
+print(z)
+
+#Convert radians → degrees
+a = math.degrees(math.pi)
+print(a)
+
+#Convert degrees → radians
+b = math.radians(180)
+print(b)
+
+'''
+
+'''
+#Exponential & Logarithmic Functions
+
+# math.exp(x)	e^x (Euler's number)
+
+x = math.exp(4)
+print(x)
+
+# math.log(x)	Natural log (base e)
+
+y = math.log(10)
+print(y)
+
+#for log10(x) algorithm
+z = math.log10(60)
+print(z)
+
+#power x^y
+
+a = math.pow(2 , 3)
+print(a)
+
+'''
+
+'''
+#n! (factorial)
+
+x = math.factorial(6)
+print(x)
+
+y = math.prod([1,2,3,4,5,6])
+print(y)
+
+l = [1.3,2.7,3.9,4.1]
+z = sum(l)
+print(z)
+
+c = math.fsum([1.3,2.7,3.9,4.1])
+print(c)
+
+'''
+
+'''
+#Advanced & Others
+
+#math.remainder(x, y)	Closest remainder
+
+x = math.remainder(17,5)
+print(x)
+
+#math.ldexp(x, i)	x * (2**i)
+
+y = math.ldexp(4 , 2)
+print(y)
+
+z = math.frexp(16)
+print(z)
+
+#math.gamma(x)	Gamma function
+
+a = math.gamma(5)
+print(a)
+
+#log gamma
+b = math.lgamma(5)
+print(b)
+
+'''
+#PI
+print(math.pi)
+
+print(math.e)
+
+print(math.tau)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
